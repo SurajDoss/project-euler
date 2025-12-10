7267421922b77eee3e36c80a30d9488823b61b6b
Suraj Doss <suraj_sebastien@yahoo.com>
2025-12-10 08:39:38 +0530

    Fix formatting and clean up print statements

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
 
-        Integer sum  = MultiplesOf3Or5.getMultiplesOf3_Or_5(1000);
-        System.out.printf("Multiples of 3 or 5 :: " + sum);
+       Integer sum  = MultiplesOf3Or5.getMultiplesOf3_Or_5(1000);
+       System.out.printf("Multiples of 3 or 5 :: " + sum);
+
+        Integer EvenFibonacciNumber  = EvenFibonacciNumbers.calculateTheSumEvenFiboncciNumbers(4000000);
+        System.out.printf("Sum of even fibonacci Number :: " + EvenFibonacciNumber);
+
     }
 }
